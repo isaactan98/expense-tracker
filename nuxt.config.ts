22881b6d6c73d2edcc240947202cb4a5f681c23c
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-05-10 21:12:39 +0800

    update: added firebase & tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  runtimeConfig: {
+    // Will be available in both server and client
+    public: {
+      // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-runtime-config
+      firebase: {
+        apiKey: process.env.NUXT_PUBLIC_API_KEY,
+        authDomain: process.env.NUXT_PUBLIC_AUTH_DOMAIN,
+        projectId: process.env.NUXT_PUBLIC_PROJECT_ID,
+        storageBucket: process.env.NUXT_PUBLIC_STORAGE_BUCKET,
+        messagingSenderId: process.env.NUXT_PUBLIC_MESSAGING_SENDER_ID,
+        appId: process.env.NUXT_PUBLIC_APP_ID,
+        measurementId: process.env.NUXT_PUBLIC_MEASUREMENT_ID,
+      },
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <NuxtWelcome />
+    </div>
+</template>
+
+<script lang="ts">
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+export default {
+    layout: 'default',
+    data: () => ({
+
+    }),
+    mounted() {
+        const config = useRuntimeConfig()
+        const firebaseConfig = {
+            apiKey: config.public['firebase'].apiKey,
+            authDomain: config.public['firebase'].authDomain,
+            projectId: config.public['firebase'].projectId,
+            storageBucket: config.public['firebase'].storageBucket,
+            messagingSenderId: config.public['firebase'].messagingSenderId,
+            appId: config.public['firebase'].appId,
+            measurementId: config.public['firebase'].measurementId
+        };
+
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+        console.info(analytics)
+    },
+}
+</script>
