0823977b190f7ac8577d891c4b5777f7bb72639d
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-06-06 21:08:43 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
             </div>
             <div class="grid place-content-center my-3">
                 <span class="text-sm text-center text-gray-500 dark:text-gray-400 mb-3">This month spend</span>
-                <span class="text-5xl my-5">$1,200.11</span>
+                <span class="text-5xl my-5">$ {{ totalExpense.toFixed(2) }}</span>
             </div>
             <div class="p-10 rounded-xl bg-black dark:bg-white"></div>
         </div>
@@ -18,7 +18,7 @@
 </template>
 
 <script lang="ts">
-
+import { collection, where, query, getDocs, onSnapshot } from 'firebase/firestore'
 export default {
     head() {
         return {
@@ -29,10 +29,45 @@ export default {
         }
     },
     data: () => ({
-
+        totalExpense: 0
     }),
-    mounted() {
-        firebase()
+    async mounted() {
+        const fb = firebase()
+        const user = await fb.getAuth()
+
+        user.onAuthStateChanged((user: any) => {
+            if (user) {
+                this.getMonthlyExpense(user.uid)
+            } else {
+                console.log('no user')
+            }
+        })
     },
+    methods: {
+        async getMonthlyExpense(userId: string | undefined) {
+            const currentDate = new Date()
+            const currentMonth = currentDate.getMonth() + 1
+            const currentYear = currentDate.getFullYear()
+
+            const startDate = `${currentYear}-${currentMonth.toString().padStart(2, '0')}-01`;
+            const endDate = `${currentYear}-${currentMonth.toString().padStart(2, '0')}-${new Date(currentYear, currentMonth, 0).getDate()}`;
+
+            const fb = firebase()
+            const ref = collection(fb.db, 'expenses')
+            this.totalExpense = 0
+            if (notNull(userId)) {
+                const q = query(ref, where('date', '>=', startDate), where('date', '<', endDate), where('userId', '==', userId))
+                onSnapshot(q, (querySnapshot) => {
+                    let total = 0
+                    querySnapshot.forEach((doc) => {
+                        // console.log(doc.data())
+                        total += doc.data().amount
+                    })
+                    this.totalExpense = total
+                })
+
+            }
+        }
+    }
 }
 </script>
