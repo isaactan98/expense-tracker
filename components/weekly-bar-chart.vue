2b9ecc75b9e39af554078dd810fd7b9245bc27f9
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-06-21 22:57:32 +0800

    update: expense index chart

---
+++ components/weekly-bar-chart.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="bg-zinc-800 dark:bg-zinc-50 p-5 rounded-2xl">
+        <div class="flex justify-between text-white dark:text-black text-sm">
+            <p class="font-bold">Analytics</p>
+            <p>This month</p>
+        </div>
+        <div class="mt-5 flex justify-between place-items-end text-white dark:text-zinc-600">
+            <div v-for="i, k, u in loopDate" class=" text-center">
+                <div class="w-3.5 bg-zinc-600 rounded-t-3xl mx-auto" :style="`height: ` + ((i / data.total) * 100) + `px`">
+                </div>
+                <span class="text-xs">{{ dayByWeek.find((e) => e.value == u)?.day }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: ['data'],
+    data: () => ({
+        dayByWeek: [
+            { day: 'Sun', value: 0 },
+            { day: 'Mon', value: 1 },
+            { day: 'Tue', value: 2 },
+            { day: 'Wed', value: 3 },
+            { day: 'Thu', value: 4 },
+            { day: 'Fri', value: 5 },
+            { day: 'Sat', value: 6 },
+        ],
+        loopDate: {
+            total: 0,
+            weekByDate: {} as any
+        }
+    }),
+    mounted() {
+        console.clear()
+        console.log("data:", this.data)
+        this.loopDate = { ...this.data.weekByDate }
+        let counter = 1
+
+        for (let w in this.loopDate.weekByDate) {
+            this.loopDate.weekByDate.w = { ...this.loopDate.weekByDate.w, day: counter }
+        }
+        console.log("loopDate: ", this.loopDate)
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,8 @@
                 <span class="text-sm text-center text-gray-500 dark:text-gray-400 mb-3">This month spend</span>
                 <span class="text-5xl my-5">$ {{ totalExpense.toFixed(2) }}</span>
             </div>
-            <div class="p-10 rounded-xl bg-black dark:bg-white"></div>
+            <!-- <div class="p-10 rounded-xl bg-black dark:bg-white"></div> -->
+            <weekly-bar-chart :data="weeklyData" v-if="!loading.weekly" />
         </div>
     </div>
 </template>
@@ -29,15 +30,24 @@ export default {
         }
     },
     data: () => ({
-        totalExpense: 0
+        totalExpense: 0,
+        weeklyData: {
+            weekByDate: [] as any,
+            total: 0,
+        },
+        loading: {
+            monthly: true,
+            weekly: true
+        }
     }),
     async mounted() {
         const fb = firebase()
         const user = await fb.getAuth()
 
-        user.onAuthStateChanged((user: any) => {
+        user.onAuthStateChanged(async (user: any) => {
             if (user) {
-                this.getMonthlyExpense(user.uid)
+                await this.getMonthlyExpense(user.uid)
+                await this.getWeeklyExpense(user.uid)
             } else {
                 console.log('no user')
             }
@@ -67,6 +77,58 @@ export default {
                 })
 
             }
+        },
+        async getWeeklyExpense(userId: string | undefined) {
+            var currentDate = new Date();
+            var currentDayOfWeek = currentDate.getDay();
+            var difference = currentDayOfWeek - 0; // Change 0 to the desired start day of the week (e.g., 1 for Monday)
+
+            var startDate = new Date(currentDate);
+            startDate.setDate(startDate.getDate() - difference);
+
+            var endDate = new Date(startDate);
+            endDate.setDate(endDate.getDate() + 6);
+
+            var startDateFormat = startDate.toLocaleDateString('en-US');
+            var endDateFormat = endDate.toLocaleDateString('en-US');
+
+            var newStartDateFormat = startDateFormat.split('/')
+            var newEndDateFormat = endDateFormat.split('/')
+
+            startDateFormat = `${newStartDateFormat[2]}-${newStartDateFormat[0].padStart(2, '0')}-${newStartDateFormat[1].padStart(2, '0')}`
+            endDateFormat = `${newEndDateFormat[2]}-${newEndDateFormat[0].padStart(2, '0')}-${newEndDateFormat[1].padStart(2, '0')}`
+
+            for (var i = 0; i < 7; i++) {
+                var loopDate = new Date(startDate);
+                loopDate.setDate(loopDate.getDate() + i);
+                var formattedDate = loopDate.toISOString().slice(0, 10);
+                this.weeklyData.weekByDate[formattedDate] = 0;
+            }
+
+            const fb = firebase()
+            const ref = collection(fb.db, 'expenses')
+
+            let processDoc = 0
+            let totalDoc = 0
+
+            if (notNull(userId)) {
+                const q = query(ref, where('date', '>=', startDateFormat), where('date', '<', endDateFormat), where('userId', '==', userId))
+                onSnapshot(q, (querySnapshot) => {
+                    totalDoc = querySnapshot.docs.length
+                    querySnapshot.forEach((doc) => {
+                        // console.log(doc.data())
+                        processDoc++
+                        this.weeklyData.weekByDate[doc.data().date] += doc.data().amount
+                        this.weeklyData.total += doc.data().amount
+
+                        if (processDoc == totalDoc) {
+                            this.loading.weekly = false
+                        }
+                    })
+                    // this.totalExpense = total
+                })
+            }
+
         }
     }
 }
