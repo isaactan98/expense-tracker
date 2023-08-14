59827cfb8c8b1b96f1c0aef7650396faba20f5b6
Isaac Tan <iztan98@gmail.com>
2023-08-14 12:40:56 +0800

    test checkl

--- pages/profile.vue
+++ pages/profile.vue
@@ -81,7 +81,8 @@ export default {
     methods: {
         async loginWithGoogle() {
             const auth = this.fb.getAuth()
-            console.warn("is PWA:: ", window.matchMedia('(display-mode: standalone)').matches);
+            // console.warn("is PWA:: ", window.matchMedia('(display-mode: standalone)').matches);
+            alert("is PWA:: " + window.matchMedia('(display-mode: standalone)').matches)
             let isPWA = window.matchMedia('(display-mode: standalone)').matches
             if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) || !isPWA) {
                 signInWithPopup(auth, new GoogleAuthProvider()).then((result) => {
